ae6d0028f36e021deedda47f25a9ebf16ff2add6
Igor Giussani <gius80@gmail.com>
2019-01-07 08:57:57 +0100

    Add config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  metamaskSeed: "excuse cloth slogan indoor online rabbit topple cancel gauge tattoo pledge noise",
+  infuraEndpoint: "https://rinkeby.infura.io/v3/83ba394c01e6471fb565461a9f6af3c5"
+}
